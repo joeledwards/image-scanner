2fc7793cd11dc22b9c3d34780e9a28beb0a8dd11
Joel Edwards <joeledwards@gmail.com>
2015-12-12 10:21:25 -0700

    Fixed a few bugs, and added .gitignore

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require("log-a-log");
 
 const _ = require("lodash");
+const async = require("async");
+const crypto = require("crypto");
+const exif = require("exif");
 const fs = require("fs");
 const os = require("os");
-const exif = require("exif");
-const walk = require("walk");
-const async = require("async");
 const tingodb = require("tingodb");
+const walk = require("walk");
 
 var home = os.homedir();
 var imageDir = `${home}/Pictures`;
@@ -15,10 +16,12 @@ var walker = walk.walk(imageDir);
 
 walker.on('file', (path, stats, next) => {
   console.log(`File: ${path}/${stats.name}`);
+  next();
 });
 
 walker.on('errors', (path, stats, next) => {
   console.log(`Error from file ${path}/${stats.name}`);
+  next();
 });
 
 walker.on('end', () => {
