7fd0d5eaeb885731eae0e7dc7eb3cbd97e559a92
Joel Edwards <joeledwards@gmail.com>
2015-12-12 14:38:31 -0700

    Switched to an async queue for processing in parallel.
    Still need to figure out why the queue gets stuck.

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ require("log-a-log");
 const _ = require("lodash");
 const async = require("async");
 const crypto = require("crypto");
+const durations = require("durations");
 const exif = require("exif");
 const fs = require("fs");
 const os = require("os");
 const tingodb = require("tingodb");
 const walk = require("walk");
 
+const concurrency = 2;
+
 var home = os.homedir();
 var imageDir = `${home}/Pictures`;
 
-var walker = walk.walk(imageDir);
+var watch = durations.stopwatch().start();
+var totalWatch = durations.stopwatch().start();
+var queued = 0;
+var processed = 0;
 
 function sha256sum (path, callback) {
   var hash = crypto.createHash('sha256');
@@ -23,17 +29,42 @@ function sha256sum (path, callback) {
   stream.on('end', () => callback(undefined, hash.digest('hex')));
 }
 
-walker.on('file', (path, stats, next) => {
+function fileHandler (path, stats, next) {
   var file = `${path}/${stats.name}`;
   sha256sum(file, (error, digest) => {
     if (error) {
-      console.error(`Error getting SHA-256 digest of ${file}`);
+      console.error(`Error getting SHA-256 digest of ${file}: ${error}`);
     }
     else {
-      console.log(`[${digest}] ${file}`);
-      next();
+      processed++;
+      if (watch.duration().millis() >= 1000) {
+        console.log(`${processed} files in ${totalWatch}`);
+        console.log(`${queue.length()} files remaining in the queue.`);
+        watch.reset().start();
+      }
+      //console.log(`[${digest}] ${file}`);
     }
+
+    next();
   })
+}
+
+function worker (task, done) {
+  var path = task.path;
+  var stats = task.stats;
+  var next = task.next;
+
+  fileHandler(path, stats, done);
+}
+
+var queue = async.queue(worker, concurrency);
+
+var walker = walk.walk(imageDir);
+
+walker.on('file', (path, stats, next) => {
+  queue.push({ path: path, stats: stats });
+  queued++;
+  next();
 });
 
 walker.on('errors', (path, stats, next) => {
@@ -42,6 +73,6 @@ walker.on('errors', (path, stats, next) => {
 });
 
 walker.on('end', () => {
-  console.log("Done.");
+  console.log(`Done. Queued ${queued} files.`);
 });
 
